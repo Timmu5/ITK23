ae45c6038a6250d644bcc061df9a594abda55e6a
Timmu5 <168183501+Timmu5@users.noreply.github.com>
2024-04-26 14:21:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,7 @@
   </head>
   <body>
     <h1>Meie projekti pealeht<h1>
+    <p>See leht arvestab ka muudatustega</p>
+    <img src="https://a.thumbs.redditmedia.com/6JGHYR1T3dEJCoRUUL4O2kS6FkcNIHvS1_-VOjFrjH8.png"
   </body>
 </html>
